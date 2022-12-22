d5f888a9912157ff360b5f1e20067953f6ffda5e
ivaylo-milanov <109028428+ivaylo-milanov@users.noreply.github.com>
2022-12-22 20:57:36 +0200

    Added the files to the repo

---
+++ src/api/api.js
@@ -0,0 +1,2 @@
+Parse.initialize("fKI0J7nzgCo7WUi5in6zOApnK0tqr3fRMWCvOnUO","4dJCyhdUE83T3kjcXzQgV2N8KgPBH9IXV1u1wv9v"); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
+Parse.serverURL = 'https://parseapi.back4app.com/';
---
+++ src/lib.js
@@ -0,0 +1,3 @@
+export { html, render, nothing} from '../node_modules/lit-html/lit-html.js';
+export { page } from '../node_modules/page/page.mjs';
+export { Parse } from '../node_modules/parse/dist/parse.min.js';

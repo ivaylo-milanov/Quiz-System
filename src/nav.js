70c62cc97ac682f7396aa2d10648e4f81f109f4e
ivaylo-milanov <109028428+ivaylo-milanov@users.noreply.github.com>
2023-01-07 16:32:30 +0200

    making the lib folder

--- src/nav.js
+++
@@ -1,8 +0,0 @@
-import { render } from "./lib.js";
-
-const root = document.getElementById('content');
-
-export function middleWare(ctx, next) {
-    ctx.render = (content) => render(content, root);
-    next();
-}
